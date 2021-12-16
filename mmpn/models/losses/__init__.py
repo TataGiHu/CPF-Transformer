95bf5a9fc696aef8ae94445161231d1c02a17203
Yupeng Li <liyupeng@momenta.ai>
2021-12-16 02:56:43 +0000

    [feat]: Split l2 loss to a separate file

--- mmpn/models/losses/__init__.py
+++ mmpn/models/losses/__init__.py
@@ -1,5 +1,6 @@
 from .bce_loss import BCELoss
-from .smooth_l1_loss import L1Loss, SmoothL1Loss, l1_loss, smooth_l1_loss,L2Loss
+from .smooth_l1_loss import L1Loss, SmoothL1Loss, l1_loss, smooth_l1_loss
+from .l2loss import L2Loss
 
 __all__ = [
     'BCELoss','SmoothL1Loss',"L2Loss"
